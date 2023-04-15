a4c8b7dd7d7b0c1afd1babcd67fd09f1e346865f
Jordi <entertainmentdutch@gmail.com>
2023-04-15 13:36:38 +0200

    V1.5
    Added 3 font suggestions under the main result.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pyfiglet
+import random
 
 app = Flask(__name__)
 
@@ -16,6 +17,29 @@ def index():
         result = figlet.renderText(text)
 
     return render_template('index.html', result=result, fonts=fonts, selected_font=selected_font)
+
+@app.route('/generate_ascii', methods=['POST'])
+def generate_ascii():
+    text = request.form.get('text', '')
+    selected_font = request.form.get('font', 'standard')
+    figlet = pyfiglet.Figlet(font=selected_font)
+    result = figlet.renderText(text)
+    return result
+
+@app.route('/font_suggestions', methods=['POST'])
+def font_suggestions():
+    num_suggestions = 3
+    text = request.form.get('text', '')
+    fonts = pyfiglet.FigletFont.getFonts()
+    suggested_fonts = random.sample(fonts, num_suggestions)
+
+    results = []
+    for font in suggested_fonts:
+        figlet = pyfiglet.Figlet(font=font)
+        result = figlet.renderText(text)
+        results.append({"font": font, "result": result})
+
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(debug=True)
